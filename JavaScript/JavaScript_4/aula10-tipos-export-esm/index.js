a0a7c5844a8eabc5b1042fbc8e639b40bc740bfd
GuilhermeRocha19 <guilhermepmrocha@gmail.com>
2024-01-31 10:29:52 -0300

    Estudos de Modulos em JavaScript Moderno

---
+++ JavaScript/JavaScript_4/aula10-tipos-export-esm/index.js
@@ -0,0 +1,13 @@
+import {inline as i} from "./inline.js"
+import defaultInline from "./inline.js"
+import { group, a } from "./non-line.js"
+import exportDefault from "./non-line.js"
+
+
+
+
+i()
+defaultInline()
+
+group()
+exportDefault()
