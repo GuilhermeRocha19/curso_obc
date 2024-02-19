3debd19344c9cd0dc4246ed2df09da932dc46142
GuilhermeRocha19 <guilhermepmrocha@gmail.com>
2024-02-19 09:05:05 -0300

    Aula de Then e Catch em JS

---
+++ JavaScript/JavaScript_6/aula06-then-e-catch/index.js
@@ -0,0 +1,26 @@
+function execute() {
+  return new Promise((resolve, reject) => {
+    console.log("Promisse sendo executada");
+    setTimeout(() => {
+
+      if (true) {
+        reject("A Promise foi rejeitada");
+      } else {
+        console.log("Resolvendo a Promise");
+        resolve(42);
+      }
+    }, 2000);
+  });
+}
+
+execute().then((result) => {
+    console.log(`O Resultado da Promise foi ${result}`);
+  }).catch((reason)=>{
+      console.log(`A Promise foi Rejeitada! Motivo: ${reason}`)
+  }).finally(()=>{
+    console.log('Promise foi finalizada!')
+  })
+
+
+
+
